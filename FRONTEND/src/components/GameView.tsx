ad7a09f280286f1d0e2d18014610e55fc9427c81
AndreCabasse <andre.cabasse.massena@gmail.com>
2025-08-10 04:19:25 +0200

    aled

--- FRONTEND/src/components/GameView.tsx
+++ FRONTEND/src/components/GameView.tsx
@@ -114,8 +114,10 @@ const GameView: React.FC = () => {
   useEffect(() => {
     trainApi.getGameState().then((state) => {
       setGameState(state);
-      const trackNumbers = Object.keys(state).map(Number); // Utilise 'state' ici, pas 'gameState'
-      if (trackNumbers.length > 0) setSelectedTrack(trackNumbers[0]);
+      if (state && typeof state === 'object') {
+        const trackNumbers = Object.keys(state).map(Number);
+        if (trackNumbers.length > 0) setSelectedTrack(trackNumbers[0]);
+      }
     });
   }, [trackCount]);
 
